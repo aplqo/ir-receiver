97d7e7d0c002a787883ebc619ace4ef83713c49c
aplqo <aplqo@outlook.com>
2019-08-08 09:30:29 +0800

    Correct lcd header

--- include/lcd.h
+++ include/lcd.h
@@ -4,7 +4,7 @@
 unsigned char lcd_read();
 void lcd_write(unsigned char rs, unsigned char data);
 
-void display_str(unsigned char pos, const char* str);
+void display_str(unsigned char pos, unsigned char length, const char* str);
 void display_uchar(unsigned char pos, unsigned char dat);
 
 #endif
