68a6d425031138925b57c523c0102eb21a657bee
aplqo <aplqo@outlook.com>
2019-08-30 16:29:22 +0800

    Remove debug functions if not debugging

--- src/debug.c
+++ src/debug.c
@@ -1,4 +1,7 @@
+#include "debug.h"
 #include "serial.h"
+
+#ifdef DEBUG
 void sec(unsigned char num)
 {
     for (unsigned char i = 0; i < 3; i++)
@@ -8,3 +11,4 @@ void sec(unsigned char num)
     send(num);
     send(0x00);
 }
+#endif
